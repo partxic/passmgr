9c286e134cacdc4fa8680a63d8a64fdfd4b0c5bd
runoneall <runoobsteve@gmail.com>
2026-06-04 20:52:29 +0800

    fix: standardize API call string formatting in saveAccount function

--- web/src/view/dashboard/AccountCreate.vue
+++ web/src/view/dashboard/AccountCreate.vue
@@ -18,7 +18,7 @@ const data = reactive({
 const saveAccount = async () => {
     try {
         loading.value = true
-        const res = await axios.post(`/api/account/set`, data)
+        const res = await axios.post('/api/account/set', data)
         ElMessage.success(res.data)
     } catch (error) {
         ElMessage.error(error.response.data)
