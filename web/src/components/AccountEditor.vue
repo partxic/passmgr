e7268cb79058f919337795369227bbb3e685921c
runoneall <runoobsteve@gmail.com>
2026-06-04 21:00:22 +0800

    refactor: remove unnecessary whitespace in AccountEditor.vue template

--- web/src/components/AccountEditor.vue
+++ web/src/components/AccountEditor.vue
@@ -20,23 +20,18 @@ defineEmits(['save', 'cancel'])
         <el-form-item label="名称">
             <el-input v-model="data.name" />
         </el-form-item>
-
         <el-form-item label="用户名">
             <el-input v-model="data.username" />
         </el-form-item>
-
         <el-form-item label="密码">
             <el-input v-model="data.password" type="password" show-password />
         </el-form-item>
-
         <el-form-item label="TOTP 密钥">
             <el-input v-model="data.totp" placeholder="可选" />
         </el-form-item>
-
         <el-form-item label="备注">
             <el-input v-model="data.note" type="textarea" placeholder="可选" />
         </el-form-item>
-
         <el-form-item>
             <el-button :loading="loading" @click="$emit('cancel')">取消</el-button>
             <el-button :loading="loading" type="primary" @click="$emit('save')">保存</el-button>
