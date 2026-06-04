14d366746c5a5fc6e37a217cda937e5e6dd679e6
runoneall <runoobsteve@gmail.com>
2026-06-04 18:30:39 +0800

    feat: refactor Dashboard component to use computed for active tab and improve tab change handling

--- web/src/view/Dashboard.vue
+++ web/src/view/Dashboard.vue
@@ -1,25 +1,27 @@
 <script setup>
 document.title = '管理面板'
 
-import { ref, onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 
-const activeName = ref('')
 const router = useRouter()
 const route = useRoute()
+const activeName = computed(() => (route.name === 'dashboard' ? 'dash-list-account' : route.name))
 
-const handleClick = (tab, _) => {
-    router.push({ name: tab.paneName })
+const onTabChange = paneName => {
+    router.push({ name: paneName })
 }
 
 onMounted(() => {
-    activeName.value = route.name
+    if (route.name === 'dashboard') {
+        router.push({ name: 'dash-list-account' })
+    }
 })
 </script>
 
 <template>
     <div class="container">
-        <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tabs v-model="activeName" @tab-change="onTabChange">
             <el-tab-pane label="账号列表" name="dash-list-account" />
             <el-tab-pane label="添加账号" name="dash-account-create" />
         </el-tabs>
