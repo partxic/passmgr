12236938b6a8d654b7e959295793a6910a963ac5
runoneall <runoobsteve@gmail.com>
2026-06-04 20:49:37 +0800

    feat: implement account creation functionality with form handling and API integration

--- web/src/view/dashboard/AccountCreate.vue
+++ web/src/view/dashboard/AccountCreate.vue
@@ -1,5 +1,39 @@
-<script setup></script>
+<script setup>
+import { ref, reactive } from 'vue'
+import { ElMessage } from 'element-plus'
+import axios from 'axios'
+import AccountEditor from '@/components/AccountEditor.vue'
+import { useRouter } from 'vue-router'
 
-<template>account create</template>
+const router = useRouter()
+const loading = ref(false)
+const data = reactive({
+    name: '',
+    username: '',
+    password: '',
+    totp: '',
+    note: ''
+})
+
+const saveAccount = async () => {
+    try {
+        loading.value = true
+        const res = await axios.post(`/api/account/set?name=${data.name}`, data)
+        ElMessage.success(res.data)
+    } catch (error) {
+        ElMessage.error(error.response.data)
+    } finally {
+        loading.value = false
+    }
+}
+
+const goAccountList = () => {
+    router.push({ name: 'dash-list-account' })
+}
+</script>
+
+<template>
+    <AccountEditor :data="data" :loading="loading" @save="saveAccount" @cancel="goAccountList" />
+</template>
 
 <style scoped></style>
