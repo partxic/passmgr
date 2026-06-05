e881d033f664ee971d6f5381154db62fd7b1bcb4
runoneall <runoobsteve@gmail.com>
2026-06-05 12:03:48 +0800

    feat: add confirmation dialog for account deletion in ListAccount component

--- web/src/view/dashboard/ListAccount.vue
+++ web/src/view/dashboard/ListAccount.vue
@@ -20,6 +20,7 @@ const filteredAccounts = computed(() => {
 })
 
 const showDialog = ref(false)
+const showConfirm = ref(false)
 const inEdit = ref(false)
 const accountData = reactive({
     name: '',
@@ -50,6 +51,7 @@ const showAccount = async account => {
 
         Object.assign(accountData, { name: account, ...res.data })
         inEdit.value = false
+        showConfirm.value = false
         showDialog.value = true
         ElMessage.success('信息获取成功')
     } catch (error) {
@@ -63,7 +65,25 @@ const updateAccount = async () => {
     try {
         loading.value = true
         const res = await axios.post('/api/account/set', accountData)
+
+        inEdit.value = false
         ElMessage.success(res.data)
+    } catch (error) {
+        ElMessage.error(error.response.data)
+    } finally {
+        loading.value = false
+    }
+}
+
+const deleteAccount = async () => {
+    try {
+        loading.value = true
+        const res = await axios.get(`/api/account/delete?name=${accountData.name}`)
+
+        showConfirm.value = false
+        showDialog.value = false
+        ElMessage.success(res.data)
+        await refresh()
     } catch (error) {
         ElMessage.error(error.response.data)
     } finally {
@@ -90,10 +110,21 @@ onMounted(refresh)
                 <div class="flex-1" />
                 <el-switch v-model="inEdit" />
                 <span>编辑</span>
+                <div class="flex-1" />
+                <el-button :loading="loading" type="danger" @click="showConfirm = true">删除</el-button>
             </div>
         </template>
         <AccountEditor v-if="inEdit" :data="accountData" :loading="loading" @save="updateAccount" @cancel="showDialog = false" />
         <AccountViewer v-else :data="accountData" />
+        <el-dialog v-model="showConfirm" align-center center :show-close="false">
+            <template #header>
+                <el-text type="danger" size="large">确定删除 {{ accountData.name }} 吗?</el-text>
+            </template>
+            <div class="flex-center">
+                <el-button :loading="loading" @click="showConfirm = false">取消</el-button>
+                <el-button :loading="loading" type="danger" @click="deleteAccount">确定</el-button>
+            </div>
+        </el-dialog>
     </el-dialog>
 </template>
 
